27dc737cb712e5faa75ce1e30c155bd710314df1
Brad McCollum <btmcco8@gmail.com>
2018-05-19 23:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function theBeatlesPlay(musicians, instruments) {  // for loop that adds musician and the instrument they play from each array to empty array
   var arr1 = [];
-  for (var x = 0; x < 4; x++) {
+  for (var x = 0; x < arr1.length; x++) {
       arr1.push(musicians[x] + " plays " + instruments[x]);
   }
   return arr1;
 }
 function johnLennonFacts(facts) { // while loop that adds "!!!" to each fact from the facts array into an empty array
-  var arr2 = [];
   var y = 0;
   while ( y < facts.length) {
     facts[y] = facts[y] + "!!!";
